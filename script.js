a40f14d3ae4e2b88ef392d88e811f1211eb77261
Tom Howard <thomasjameshoward@gmail.com>
2021-01-06 11:09:58 -0500

    code prompts user and generates password at their desired length based on uppercase, with no special or numbers at this point

---
+++ script.js
@@ -0,0 +1,31 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+ 
+generateBtn.addEventListener("click", writePassword);
+
+
+
+
+function writePassword() {
+  var total = prompt("Choose a password length between 8 and 128");
+  confirm ("Please type yes or no on the following prompts to select your password requirements");
+  var lowcase = prompt("Is an uppercase letter required?");
+  var neednum = prompt("Is a number required?");
+  var special = prompt("Is a special character required?");
+
+  var lcharSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var ucharSet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var scharSet = ["!", "@", "#", "$", "%", "^", "&"];
+  var ncharSet = [1,2,3,4,5,6,7,8,9];
+  
+  
+if (lowcase === "yes" && neednum === "no" && special === "no") {  var charSet = ucharSet[Math.floor(Math.random()*ucharSet.length)];
+
+while (charSet.length<total) {
+  charSet +=lcharSet[Math.floor(Math.random()*lcharSet.length)];
+}
+}
+console.log(charSet)
+
+}
